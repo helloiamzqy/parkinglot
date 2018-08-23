5d55e59e2224fdc63fc5d352fa2e96dd1d4b4db5
ZHENGDU <dunn.zheng@oocl.com>
2018-08-23 19:22:09 +0800

    clean code second

--- src/test/java/parkinglot/ParkingBoyTest.java
+++ src/test/java/parkinglot/ParkingBoyTest.java
@@ -1,6 +1,7 @@
 package parkinglot;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingBoyTest {
@@ -8,18 +9,19 @@ public class ParkingBoyTest {
     @Test
     public void should_return_true_when_input_car(){
         //Given
-        Car car = new Car();
-        car.setCarNum("粤C123123123");
+        Car car = new Car("粤C123123123");
         ParkLot parkLot = new ParkLot();
         parkLot.setCarportCount(1);
-        parkLot.park(new Car("粤C22233322"));
+        Car parkedCar = new Car("粤C3213213");
+        parkLot.park(parkedCar);
         ParkLot parkLot1 = new ParkLot();
         parkingBoy.addParkLots(parkLot);
         parkingBoy.addParkLots(parkLot1);
         //When
         parkingBoy.park(car);
         //Then
-        Assert.assertEquals(car,parkLot1.getCarMap().get(car.getCarNum()));
+        Assert.assertEquals(parkedCar,parkLot.pick(parkedCar.getCarNum()));
+        Assert.assertEquals(car,parkLot1.pick(car.getCarNum()));
     }
     @Test
     public void should_return_car_when_input_carNum(){
